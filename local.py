6ae14aaceeabe97d60c676145bef156c464ff185
Ayesha-Qadir-code <ayeshaqadir2252005@gmail.com>
2025-04-24 23:44:57 +0500

    gitignore

--- local.py
+++ local.py
@@ -1,3 +1,8 @@
 print("Hello World kyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 print("new feature added and after that feature 1 also")
-print("hello from feature1")
+print("Hello from feature1")
+print("nothing")
+print("Hello from main")
+
+
+
