757bc78bf9d4c3b7849e567f50fc5b33db1875f6
Ayesha-Qadir-code <ayeshaqadir2252005@gmail.com>
2025-04-24 20:35:48 +0500

    bahi newww

--- local.py
+++ local.py
@@ -1,2 +1,2 @@
-print("Hello World")
+print("Hello World kyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa kronnnnnnnnnnnnnnnnn")
 print("new feature added and after that feature 1 also")
